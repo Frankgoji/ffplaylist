b5f0d365d09cadf57d65efcf1d756f189ee2539b
Franklin Lee <frankgoji@gmail.com>
2017-04-22 16:28:43 -0700

    Can now edit in the middle of playing the playlist

--- play_list.sh
+++ play_list.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-while read l; do
-    ffplay -autoexit "$l"
-done < <(cat "$1")
+# Use index so that one can make changes to the playlist file while playing
+i=1
+numlines=$(wc "$1" | cut -d" " -f3)
+while [[ $i -le $numlines ]]; do
+    ffplay -autoexit "$(sed -n ""$i"p" "$1")"
+    numlines=$(wc "$1" | cut -d" " -f3)     # Update step in case edit occurs
+    i=$((i+1))
+done
